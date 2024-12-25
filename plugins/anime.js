117a919e977b8534f669c8897491d9f942a19179
David is trust worthy 🚀 <davidpromise2003@gmail.com>
2024-12-25 01:01:59 +0100

    Add files via upload

---
+++ plugins/anime.js
@@ -0,0 +1,142 @@
+
+
+const { cmd, commands } = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+const fs = require('fs');
+const axios = require('axios')
+var imgmsg = "*Give me a anime name !*"
+var descgs = "It gives details of given anime name."
+var cants = "I cant find this anime."
+
+cmd({
+    pattern: "loli",
+    alias: ["imgloli"],
+    react: '🧧',
+    desc: "Download anime loli images.",
+    category: "anime",
+    use: '.loli',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let res = await axios.get('https://api.lolicon.app/setu/v2?num=1&r18=0&tag=lolicon')
+let wm = `🧧 Random loli image
+
+> *@CREATE-BY-MALVIN..*`
+await conn.sendMessage(from, { image: { url: res.data.data[0].urls.original }, caption: wm}, { quoted: mek })
+} catch (e) {
+reply(cants)
+console.log(e)
+}
+})
+
+cmd({
+    pattern: "waifu",
+    alias: ["imgwaifu"],
+    react: '🧧',
+    desc: "Download anime waifu images.",
+    category: "anime",
+    use: '.waifu',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let res = await axios.get('https://api.waifu.pics/nsfw/waifu')
+let wm = `🧧 Random Waifu image
+
+> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ x-ʙᴏᴛ-ᴍᴅ..*`
+await conn.sendMessage(from, { image: { url: res.data.url }, caption: wm}, { quoted: mek })
+} catch (e) {
+reply(cants)
+console.log(e)
+}
+})
+
+cmd({
+    pattern: "neko",
+    alias: ["imgneko"],
+    react: '🧧',
+    desc: "Download anime neko images.",
+    category: "anime",
+    use: '.neko',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let res = await axios.get('https://api.waifu.pics/nsfw/neko')
+let wm = `🧧 Random neko image
+
+> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ x-ʙᴏᴛ-ᴍᴅ*`
+await conn.sendMessage(from, { image: { url: res.data.url  }, caption: wm}, { quoted: mek })
+} catch (e) {
+reply(cants)
+console.log(e)
+}
+})
+  
+cmd({
+    pattern: "foxgirl",
+    alias: ["imgmegumin"],
+    react: '🧧',
+    desc: "Download anime megumin images.",
+    category: "anime",
+    use: '.megumin',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let res = await axios.get('https://nekos.life/api/v2/img/fox_girl')
+let wm = `🧧 Random fox girl image
+
+> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ x-ʙᴏᴛ-ᴍᴅ...*`
+await conn.sendMessage(from, { image: { url: res.data.url }, caption: wm}, { quoted: mek })
+} catch (e) {
+reply(cants)
+console.log(e)
+}
+})
+
+cmd({
+    pattern: "maid",
+    alias: ["imgmaid"],
+    react: '🧧',
+    desc: "Download anime maid images.",
+    category: "anime",
+    use: '.maid',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let res = await axios.get('https://api.waifu.im/search/?included_tags=maid')
+let wm = `🧧 Random maid image
+
+> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ x-ʙᴏᴛ-ᴍᴅ*`
+await conn.sendMessage(from, { image: { url: res.data.images[0].url  }, caption: wm}, { quoted: mek })
+} catch (e) {
+reply(cants)
+console.log(e)
+}
+})
+
+cmd({
+    pattern: "gwallp",
+    alias: ["girlwallpaper"],
+    react: '🧧',
+    desc: "Download anime wallpaper images.",
+    category: "anime",
+    use: '.awoo',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let res = await axios.get('https://nekos.life/api/v2/img/wallpaper')
+let wm = `🧧 Random wallpaper image
+
+> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ x-ʙᴏᴛ-ᴍᴅ*`
+await conn.sendMessage(from, { image: { url: res.data.url }, caption: wm}, { quoted: mek })
+} catch (e) {
+reply(cants)
+console.log(e)
+}
+})
