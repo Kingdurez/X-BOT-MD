f58d1066d4e6120076334361dd01420d14ef5166
David is trust worthy 🚀 <davidpromise2003@gmail.com>
2024-12-26 15:31:07 +0100

    Update fun2.js

--- plugins/fun2.js
+++ plugins/fun2.js
@@ -2,6 +2,59 @@ const axios = require('axios');
 const { cmd } = require('../command');
 
 cmd({
+    pattern: "facts",
+    desc: "🧠 Get a random fun fact",
+    react: "😝",
+    category: "other",
+    filename: __filename
+},
+async (conn, mek, m, { from, q, reply }) => {
+    try {
+        const url = 'https://nekos.life/api/v2/fact';  // API for random facts
+        const response = await axios.get(url);
+        const fact = response.data.fact;
+
+        const funFact = `
+  *X-BOT-MD RANDOM FACT*
+
+${fact}
+
+
+`;
+
+        return reply(funFact);
+    } catch (e) {
+        console.log(e);
+        return reply("⚠️ An error occurred while fetching a fun fact. Please try again later🤕.");
+    }
+});
+
+cmd({
+    pattern: "joke2",
+    desc: "😂 Get a random joke",
+    react: "🤣",
+    category: "random",
+    filename: __filename
+},
+async (conn, mek, m, { from, q, reply }) => {
+    try {
+        const url = 'https://v2.jokeapi.dev/joke/Any?type=single"';  // API for random jokes
+        const response = await axios.get(url);
+        const joke = response.data;
+        const jokeMessage = `
+  *As requested* 
+*${joke.joke}*
+${Category.category} 😄
+> *POWERED BY X-BOT-MD*
+`;
+        return reply(jokeMessage);
+    } catch (e) {
+        console.log(e);
+        return reply("⚠️ Couldn't fetch a joke right now. Please try again later.");
+    }
+});
+
+cmd({
     pattern: "rizz",
     desc: "Get a random pickup line",
     react: "🫠",
@@ -41,7 +94,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const response = await axios.get(url);
         const question = data.results[0].question;
 
-        const qmessage = `
+        const qMessage = `
   *X-BOT-MD RANDOM QUESTION*
 
 ${question}
@@ -49,7 +102,7 @@ ${question}
 
 `;
 
-        return reply(question);
+        return reply(qMessage);
     } catch (e) {
         console.log(e);
         return reply("⚠️ An error occurred while fetching a question. Please try again later🤕.");
@@ -66,9 +119,9 @@ async (conn, mek, m, { from, q, reply }) => {
     try {
         const url = 'https://api.truthordarebot.xyz/v1/truth';  // API for random trurh
         const response = await axios.get(url);
-        const truthQuestion = data.question;
+        const truth = data.question;
 
-        const truthmessage = `
+        const truthQuestion = `
   *X-BOT-MD RANDOM TRUTH*
 
 ${truthQuestion}
@@ -93,9 +146,9 @@ async (conn, mek, m, { from, q, reply }) => {
     try {
         const url = 'https://api.truthordarebot.xyz/v1/dare';  // API for random dare
         const response = await axios.get(url);
-        const dareChallenge = data.question;
+        const dare = data.question;
 
-        const daremessage = `
+        const dareChallenge = `
   *X-BOT-MD RANDOM DARE*
 
 ${dareChallenge}
