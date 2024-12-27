2629f614d840c9f4b3378488d5c152f05f5a0a16
David is trust worthy 🚀 <davidpromise2003@gmail.com>
2024-12-28 00:56:21 +0100

    Update singer.js

--- plugins/singer.js
+++ plugins/singer.js
@@ -1,4 +1,5 @@
 const config = require('../config')
+const fetch = require('node-fetch')
 const {cmd , commands} = require('../command')
 const { fetchJson } = require('../lib/functions')
 
@@ -12,18 +13,37 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let data = await fetchJson(`https://itzpire.com/search/lyrics?query=${q}`)
-return reply(` 
- X-BOT-MD LYRICS FINDER 
- 
- *STATUS* ${data.status}
- *TITLE* ${data.data.title} 
-        *SONG* 
-      ${data.data.lyrics}
-      
-  *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴀᴠɪᴅx*`)
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+if (!q) {
+        return await m.send("*_Please provide a song name!_*");
+      }
+
+      const apiUrl = `https://itzpire.com/search/lyrics?query=${encodeURIComponent(q)}`;
+      const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+        return await m.send(
+          `*_Error: ${response.status} ${response.statusText}_*`
+        );
+      }
+
+      const data = await response.json();
+
+      if (data.status !== "success") {
+        return await m.send("*_An error occurred while fetching the data._*");
+      }
+
+      const { artist, lyrics, title } = data.data;
+
+      const lyricsMessage =`
+*Song:* ${title}
+*Artist:* ${artist}
+
+${lyrics}
+`;
+
+      await m.send(lyricsMessage);
+    } catch (e) {
+      await m.error(`${e}\n\ncommand: lyrics`, e);
+    }
+  }
+);
