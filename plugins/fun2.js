f5c9f2712c5a5d6b5817566152b2df6a72195889
David is trust worthy 🚀 <davidpromise2003@gmail.com>
2024-12-27 17:54:08 +0100

    Update fun2.js

--- plugins/fun2.js
+++ plugins/fun2.js
@@ -189,4 +189,35 @@ ${why}
         return reply("⚠️ An error occurred while fetching a why question. Please try again later🤕.");
     }
 });
+
+
+cmd({
+    pattern: "insult",
+    desc: "Get a random insult",
+    react: "🤔",
+    category: "fun",
+    filename: __filename
+},
+async (conn, mek, m, { from, q, reply }) => {
+    try {
+        const url = 'https://evilinsult.com/generate_insult.php?lang=en&type=json';  // API for random why
+        const response = await axios.get(url);
+        const insult = response.data.insult;
+
+        const insultMessage = `
+  *X-BOT-MD RANDOM INSULT*
+
+${insult}
+
+
+`;
+
+        return reply(insultMessage);
+    } catch (e) {
+        console.log(e);
+        return reply("⚠️ An error occurred while fetching a insult. Please try again later🤕.");
+    }
+});
+
+
     
